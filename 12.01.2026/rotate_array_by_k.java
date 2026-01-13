79b6b18567899cefd2427caf5acf5c1e5d8c1e1e
subhasiniravikumar <151342817+subhasiniravikumar@users.noreply.github.com>
2026-01-13 10:16:04 +0530

    Create rotate_array_by_k.java

---
+++ 12.01.2026/rotate_array_by_k.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int k = sc.nextInt();
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+
+    static void reverse(int[] arr, int l, int r) {
+        while (l < r) {
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
