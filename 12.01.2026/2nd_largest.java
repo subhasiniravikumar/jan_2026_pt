47ed90cb09f616b95d4f36a69997cf0962ddbaa0
subhasiniravikumar <151342817+subhasiniravikumar@users.noreply.github.com>
2026-01-13 10:14:48 +0530

    Implement program to find the second largest number

---
+++ 12.01.2026/2nd_largest.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                second = largest;
+                largest = num;
+            } else if (num > second && num < largest) {
+                second = num;
+            }
+        }
+
+        if (second == Integer.MIN_VALUE)
+            System.out.println("Second largest not found");
+        else
+            System.out.println("Second largest: " + second);
+    }
+}
